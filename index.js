61563dc40831f06e8881b7851debb62073f6d5ac
jundran <jundran@example.com>
2022-10-24 10:05:59 -0600

    Tidy up code logic and fix null error if user cancels prompt

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"]
   return choices[Math.floor(Math.random() * choices.length)]
@@ -12,19 +13,15 @@ function playRound(playerChoice, computerChoice) {
 
   let winner, message
 
-  if(playerChoice === computerChoice) {
+  if (playerChoice === computerChoice) {
     winner = null
     message = `It's a draw. Both you and the computer chose ${playerChoice}.`
   }
-  else if(playerChoice === "rock" && computerChoice === "scissors") {
-    winner = "player"
-    message =  `You win! ${LOGIC[playerChoice]}`
-  }
-  else if(playerChoice === "paper" && computerChoice === "rock") {
-    winner = "player"
-    message =  `You win! ${LOGIC[playerChoice]}`
-  }
-  else if(playerChoice === "scissors" && computerChoice === "paper") {
+  else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
     winner = "player"
     message =  `You win! ${LOGIC[playerChoice]}`
   }
@@ -33,10 +30,7 @@ function playRound(playerChoice, computerChoice) {
     message =  `You lose. ${LOGIC[computerChoice]}`
   }
 
-  return {
-    winner,
-    message
-  }
+  return { winner, message }
 }
 
 function game() {
@@ -44,36 +38,33 @@ function game() {
     player: 0,
     computer: 0
   }
-   
+  
   // Play game
   for (let i = 0; i < 5; i++) {
     // Get user input
     let playerChoice = null
-    while(!playerChoice) {
-      const userInput = prompt("Choose your weapon: rock, paper or scissors.").trim().toLowerCase()
-      if(!["rock", "paper", "scissors"].includes(userInput)) {
-        console.log("Invalid input. Try again.")
-      } 
-      else {
-        playerChoice = userInput
-      } 
+    while (!playerChoice) {
+      const p = prompt("Choose your weapon: rock, paper or scissors.")
+      const userInput = p && p.trim().toLowerCase()
+      if (!["rock", "paper", "scissors"].includes(userInput)) console.log("Invalid input. Try again.")
+      else playerChoice = userInput
     }
 
     // Play round
     const round = playRound(playerChoice, getComputerChoice())
-    if(round.winner) score[round.winner]++
+    if (round.winner) score[round.winner]++
     console.log(`Round ${i+1}: ${round.message}`)
   }
 
   // Game over - print results
-  if(score.player === score.computer) {
+  if (score.player === score.computer) {
     console.log("Game over. It was a draw.")
   }
   else {
     const winner = score.player > score.computer ? "You" : "The computer"
-    console.log(`Game over. ${winner} won the game.`)
+    console.log(`Game over. ${winner} won the game!`)
   }
-  console.log(`Score -- [Player: ${score.player}] [Computer: ${score.computer}]`)
+  console.log(`Score: [Player: ${score.player}] [Computer: ${score.computer}]`)
 }
 
 game()
